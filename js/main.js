cc186c4f28cafc333c7d61d052a21cf87b38bf78
bohdan <bshevyrov@gmail.com>
2023-10-28 23:06:34 +0300

    added local storage

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 'use strict';
 
+Storage.prototype.getObj = function(key) {
+    return JSON.parse(this.getItem(key))
+}
+
 let goTopButton = document.getElementById("go-top-btn-content");
 let currentPosition = 0;
 
@@ -45,4 +49,30 @@ function colorChangeFunction(e) {
 }
 
 
-localStorage.setItem()
+
+// data.forEach(element =>console.log(element));
+
+function getDataFromLocalStorage() {
+    let rsl=[];
+    let data = new Array(localStorage.getObj("dataArray")).pop();
+
+    for (let i = 0; i <data.length; i++) {
+        rsl.push(new Map(Object.entries(data[i])));
+    }
+    console.log(rsl.length);
+rsl.sort(compareFn);
+rsl.forEach(e=>console.log(e));
+}
+
+
+function compareFn(a, b) {
+  if(  a instanceof Map && b instanceof Map){
+     let dateA =Date.parse(a.get("created"));
+     let dateB =Date.parse(b.get("created"));
+     return dateB-dateA;
+  }
+    // a must be equal to b
+    return 0;
+}
+
+getDataFromLocalStorage();
