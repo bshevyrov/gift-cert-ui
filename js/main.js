914bd260af96c20353ca5cfad32224f898ea9f0f
bohdan <bshevyrov@gmail.com>
2023-10-28 17:59:58 +0300

    optional added

--- js/main.js
+++ js/main.js
@@ -3,17 +3,19 @@
 let goTopButton = document.getElementById("go-top-btn-content");
 let currentPosition = 0;
 
-window.onscroll = function () {displayFunction()};
+window.onscroll = function () {
+    displayFunction()
+};
 
 function displayFunction() {
-    if(document.documentElement.scrollTop > 20 && currentPosition === 0) {
+    if (document.documentElement.scrollTop > 20 && currentPosition === 0) {
         goTopButton.style.display = "block";
         goTopButton.style.top = "";
         goTopButton.style.bottom = "5%";
-    } else if(document.documentElement.scrollTop < 20 && currentPosition === 0) {
+    } else if (document.documentElement.scrollTop < 20 && currentPosition === 0) {
         goTopButton.style.display = "none";
     }
-    if(currentPosition !== 0) {
+    if (currentPosition !== 0) {
         goTopButton.style.bottom = "";
         goTopButton.style.top = "15%";
     }
@@ -21,7 +23,7 @@ function displayFunction() {
 }
 
 function scrollFunction() {
-    if(currentPosition === 0) {
+    if (currentPosition === 0) {
         currentPosition = document.documentElement.scrollTop;
         document.documentElement.scrollTop = 0;
     } else {
@@ -30,3 +32,17 @@ function scrollFunction() {
     }
 }
 
+// document.addEventListener("click",changeColor(e))
+
+function colorChangeFunction(e) {
+
+    let heartBtn = e.target;
+    if (heartBtn.style.color === "red") {
+        heartBtn.style.color = "#555555";
+    } else {
+        heartBtn.style.color = "red";
+    }
+}
+
+
+localStorage.setItem()
